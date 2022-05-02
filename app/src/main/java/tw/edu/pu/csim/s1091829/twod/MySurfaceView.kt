a2a47b8e73600692744b83a4892ea69ebc70547e
unknown <s1091829>
2022-05-03 00:22:06 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/csim/s1091829/twod/MySurfaceView.kt
@@ -0,0 +1,21 @@
+package tw.edu.pu.csim.s1091829.twod
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback{
+    override fun surfaceCreated(p0: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+
+    }
+}
